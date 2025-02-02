5e4cfa3558cb41e06dd24468f23b9c1ab00d3a47
29nikki <nikkirao2907@gmail.com>
2025-02-02 16:38:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+
+function generateQR(){
+    if(qrText.value.length > 0){
+        qrImage.src = URL + qrText.value;
+        console.log("Generating QR Code");
+        imgBox.classList.add("show-img");
+    }
+    else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+           qrText.classList.remove("error"); 
+        },1000);
+    }
+}
+
