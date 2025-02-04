20ee5e1db799ada05468e00f00dbb8387150c9c3
29nikki <nikkirao2907@gmail.com>
2025-02-04 23:31:42 +0530

    my second commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let downloadBtn = document.getElementById("downloadBtn");
 
 
 function generateQR(){
@@ -10,6 +11,7 @@ function generateQR(){
         qrImage.src = URL + qrText.value;
         console.log("Generating QR Code");
         imgBox.classList.add("show-img");
+        downloadBtn.style.display = "block";
     }
     else{
         qrText.classList.add('error');
@@ -17,5 +19,12 @@ function generateQR(){
            qrText.classList.remove("error"); 
         },1000);
     }
+}
+
+function downloadQR() {
+    let link = document.createElement('a');
+    link.href = qrImage.src;
+    link.download = 'qrcode.png';
+    link.click();
 }
 
